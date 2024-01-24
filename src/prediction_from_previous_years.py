cb7828da7661eac2b02c7935327bdb06aeec4b49
paks <liza.dobretsova.30@gmail.com>
2024-01-24 20:03:14 +0300

    add UI for unconditional prediction

--- src/prediction_from_previous_years.py
+++ src/prediction_from_previous_years.py
@@ -11,10 +11,36 @@ def predict_share_from_coefficients(year: int, tan: float, bias: float) -> float
 
 
 def main():
-    coefficients = read_coefficients_from_csv("../output/coefficients.csv")
-    rows_india = coefficients.loc[coefficients["Entity"] == "India"]
+    entities = ['Americas (excl. USA)', 'Asia (excl. China and India)', 'China',
+                'Europe', 'India', 'Middle East & North Africa', 'Oceania',
+                'Sub-Saharan Africa', 'United States']
+    print("Choose an entity from following list: ")
 
-    print(predict_share_from_coefficients(2019, rows_india["Tan"].iloc[0], rows_india["Bias"].iloc[0]))
+    current_year = 2024
+
+    for entity in entities:
+        if entity != "United States":
+            print(entity, end=", ")
+        else:
+            print(entity)
+
+    chosen_entity = input()
+    if chosen_entity not in entities:
+        print("No such entity")
+        return
+    print("Input a year: ")
+    chosen_year = int(input())
+    coefficients = read_coefficients_from_csv("../output/coefficients.csv")
+    rows = coefficients.loc[coefficients["Entity"] == chosen_entity]
+    unconditional_res = predict_share_from_coefficients(chosen_year, rows["Tan"].iloc[0], rows["Bias"].iloc[0])
+    rounded_unconditional_res = '%.3f' % unconditional_res
+    if chosen_year >= current_year:
+        verb_form = "will be"
+    else:
+        verb_form = "was"
+    print("According to unconditional prediction, шn", chosen_year, "the share of recycled plastic in", chosen_entity,
+          verb_form,
+          rounded_unconditional_res, "%")
 
 
 if __name__ == "__main__":
